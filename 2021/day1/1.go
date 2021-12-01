90551542e2b53f92da0559ea7300e72f8054835a
Atef Arfaoui <arfatef@gmail.com>
2021-12-01 08:06:06 +0100

    day1 part2

--- 2021/day1/1.go
+++ 2021/day1/1.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 )
 
-func main() {
+func part1() {
 	file, err := os.Open("2021/day1/input.txt")
 	if err != nil {
 		panic(err)
@@ -28,4 +28,35 @@ func main() {
 		prev = num
 	}
 	println(inremental)
+}
+
+func part2() {
+	file, err := os.Open("2021/day1/input.txt")
+	if err != nil {
+		panic(err)
+	}
+	var nums []int
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		num, _ := strconv.Atoi(line)
+		nums = append(nums, num)
+	}
+
+	prev := nums[0] + nums[1] + nums[2]
+	inremental := 0
+
+	for i := 1; i < len(nums)-2; i += 1 {
+		current := nums[i] + nums[i+1] + nums[i+2]
+		if current > prev {
+			inremental += 1
+		}
+		prev = current
+	}
+	println(inremental)
+}
+
+func main() {
+	part1()
+	part2()
 }
