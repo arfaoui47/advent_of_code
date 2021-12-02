c1209c794ad05bd3a0a93af54ad75d04c5ab35e1
Atef Arfaoui <arfatef@gmail.com>
2021-12-02 07:47:53 +0100

    day2

---
+++ 2021/day2/2.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func part1() {
+	file, err := os.Open("2021/day2/input.txt")
+	if err != nil {
+		panic(err)
+	}
+	// var m map[string]int
+	scanner := bufio.NewScanner(file)
+	horizontal := 0
+	vertical := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		v := strings.Split(line, " ")
+		direction := v[0]
+		value, _ := strconv.Atoi(v[1])
+		if direction == "forward" {
+			horizontal += value
+		} else if direction == "down" {
+			vertical += value
+		} else if direction == "up" {
+			vertical -= value
+		}
+	}
+	println(horizontal * vertical)
+}
+
+func part2() {
+	file, err := os.Open("2021/day2/input.txt")
+	if err != nil {
+		panic(err)
+	}
+	// var m map[string]int
+	scanner := bufio.NewScanner(file)
+	horizontal := 0
+	vertical := 0
+	aim := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		v := strings.Split(line, " ")
+		direction := v[0]
+		value, _ := strconv.Atoi(v[1])
+		if direction == "forward" {
+			horizontal += value
+			vertical += aim * value
+		} else if direction == "down" {
+			aim += value
+		} else if direction == "up" {
+			aim -= value
+		}
+	}
+	println(horizontal * vertical)
+}
+
+func main() {
+	part1()
+	part2()
+}
