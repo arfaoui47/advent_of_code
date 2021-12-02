e2229e42d87fa41cb489d81bfabbc8e9981fe31a
Atef Arfaoui <arfaoui47@gmail.com>
2021-12-02 21:12:14 +0100

    day1 part2 in rust

--- 2021/day1/rust_day1/src/main.rs
+++ 2021/day1/rust_day1/src/main.rs
@@ -3,6 +3,11 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
+    part1();
+    part2();
+}
+
+fn part1() {
     // File hosts must exist in current path before this produces output
     let mut v = Vec::new();
     if let Ok(lines) = read_lines("../../input.txt") {
@@ -16,18 +21,41 @@ fn main() {
     let mut prev = v[0];
     let mut incr = 0;
     for i in &v {
-        if *i > prev { 
+        if *i > prev {
             incr += 1;
         }
         prev = *i;
     }
-    print!("{}", incr);
+    println!("{}", incr);
 }
 
-// The output is wrapped in a Result to allow matching on errors
-// Returns an Iterator to the Reader of the lines of the file.
+fn part2() {
+    // File hosts must exist in current path before this produces output
+    let mut v = Vec::new();
+    if let Ok(lines) = read_lines("../../input.txt") {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(number) = line {
+                v.push(number.parse::<i32>().unwrap());
+            }
+        }
+    }
+    let mut prev = v[0] + v[1] + v[2];
+    let mut incr = 0;
+    let mut current;
+    for i in 1..v.len() - 2 {
+        current = v[i] + v[i + 1] + v[i + 2];
+        if current > prev {
+            incr += 1;
+        }
+        prev = current;
+    }
+    print!("{}", incr);
+}
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
