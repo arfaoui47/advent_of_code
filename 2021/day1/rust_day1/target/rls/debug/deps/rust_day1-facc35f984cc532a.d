1b73b175f8a4db66abfd0cfb60d4374834f5419f
Atef Arfaoui <arfaoui47@gmail.com>
2021-12-02 20:35:53 +0100

    add rust files

---
+++ 2021/day1/rust_day1/target/rls/debug/deps/rust_day1-facc35f984cc532a.d
@@ -0,0 +1,5 @@
+/home/atef/Desktop/advent_of_code/2021/day1/rust_day1/target/rls/debug/deps/rust_day1-facc35f984cc532a.rmeta: src/main.rs
+
+/home/atef/Desktop/advent_of_code/2021/day1/rust_day1/target/rls/debug/deps/rust_day1-facc35f984cc532a.d: src/main.rs
+
+src/main.rs:
