eca0e2b01a8b8bc328437550554c4a4f714769a9
Atef Arfaoui <arfaoui47@gmail.com>
2021-12-08 16:48:54 +0100

    day7 part2in go

--- 2021/day7/main.go
+++ 2021/day7/main.go
@@ -42,6 +42,40 @@ func part1() {
 	fmt.Println(min_fuels)
 }
 
+func part2() {
+	file, err := os.Open("2021/day7/input.txt")
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+	fuels := []int{}
+	fuels_st := []string{}
+	for scanner.Scan() {
+		line := scanner.Text()
+		fuels_st = strings.Split(line, ",")
+	}
+	for i := 0; i < len(fuels_st); i++ {
+		fuel, _ := strconv.Atoi(fuels_st[i])
+		fuels = append(fuels, fuel)
+	}
+	min_fuels := int(math.Pow(10, 10))
+	sort.Ints(fuels)
+	min_ := fuels[0]
+	max_ := fuels[len(fuels)-1]
+	for i := min_; i <= max_; i++ {
+		sum := 0
+		for j := 0; j < len(fuels); j++ {
+			for k := 0; k < Abs(fuels[j]-i)+1; k++ {
+				sum += k
+			}
+		}
+		if sum < min_fuels {
+			min_fuels = sum
+		}
+	}
+	fmt.Println(min_fuels)
+}
+
 func Abs(x int) int {
 	if x < 0 {
 		return -x
@@ -51,4 +85,5 @@ func Abs(x int) int {
 
 func main() {
 	part1()
+	part2()
 }
