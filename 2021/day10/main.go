1bf49f934e56870f87aa190d981176ab1597fa40
Atef Arfaoui <arfaoui47@gmail.com>
2021-12-11 00:02:38 +0100

    day 10 part2

--- 2021/day10/main.go
+++ 2021/day10/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 )
 
 func part1() {
@@ -43,6 +44,59 @@ func part1() {
 	}
 	fmt.Println(resut)
 }
+func part2() {
+	file, err := os.Open("2021/day10/input.txt")
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+	corrupted := false
+	total_scores := make([]int, 0)
+	stack := make([]string, 0)
+	mapping := map[string]string{"{": "}", "[": "]", "(": ")", "<": ">"}
+	for scanner.Scan() {
+		line := scanner.Text()
+		corrupted = false
+		stack = make([]string, 0)
+		for _, char := range line {
+			if char == '{' || char == '[' || char == '(' || char == '<' {
+				stack = append(stack, string(char))
+			} else if char == '}' || char == ']' || char == ')' || char == '>' {
+				if len(stack) == 0 {
+					panic("stack is empty")
+				}
+				if mapping[stack[len(stack)-1]] != string(char) {
+					corrupted = true
+					break
+				}
+				stack = stack[:len(stack)-1]
+			}
+		}
+		score := 0
+		var v int
+		if !corrupted {
+			for i := len(stack) - 1; i >= 0; i-- {
+				if stack[i] == "(" {
+					v = 1
+				} else if stack[i] == "[" {
+					v = 2
+				} else if stack[i] == "{" {
+					v = 3
+				} else if stack[i] == "<" {
+					v = 4
+				}
+				score = 5*score + v
+			}
+			total_scores = append(total_scores, score)
+		}
+
+	}
+
+	sort.Ints(total_scores)
+	fmt.Println(total_scores[len(total_scores)/2])
+
+}
 func main() {
-	part1()
+	// part1()
+	part2()
 }
